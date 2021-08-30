e398dfc402c1ecc70d6474851944bd17287d6955
theskyxsea <87428292+theskyxsea@users.noreply.github.com>
2021-08-30 22:50:25 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,10 @@
+class Demo
+{
+	public static void main(String[] args)
+	{
+		System.out.println("java");
+		System.out.println(125);
+		System.out.println(152.79);
+		System.out.println(true);
+	}
+}
---
+++ OperatorDemo1.java
@@ -0,0 +1,21 @@
+class OperatorDemo1
+{
+	public static void main (String[] arg)
+	{
+		System.out.println(5+5);
+		System.out.println(22.5+36.4);
+		System.out.println(29+34.65);
+
+		System.out.println("the"+"skyxsea");
+		System.out.println("only"+25);
+		System.out.println(39+"boomboom");
+		System.out.println(20+5+"java");
+		System.out.println("java"+20+5);
+		System.out.println("java"+(20+5));
+
+		System.out.println('A'+'B');
+		System.out.println('A'+10);
+		System.out.println('C'+"java");
+		System.out.println("java"+"developer");
+	}
+}
