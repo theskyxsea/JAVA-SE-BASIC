991404a46ef295f92b38ca4c985ebb9f43015e7a
theskyxsea <87428292+theskyxsea@users.noreply.github.com>
2021-09-02 20:16:56 +0530

    java-variable-pratice
    I am Learning Java from Scratch, Here's I'm uploading my daily java Practice.

---
+++ DemoVariable4.java
@@ -0,0 +1,12 @@
+class DemoVariable4
+{
+	public static void main (String[] args)
+	{
+		int n1=10, n2=20, n3=30;
+		char ch1='A', ch2='B', ch3='C';
+		System.out.println(n1+"\t"+n2+"\t"+n3);
+		System.out.println(ch1+"\t"+ch2+"\t"+ch3);
+		System.out.println("------------------------------");
+		System.out.println((n1+ch1)+"\t"+(n2+ch2)+"\t"+(n3+ch3));
+	}
+}
